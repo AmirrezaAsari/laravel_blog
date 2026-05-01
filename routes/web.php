fa6ec2f7e58aef40d8600e0c45ca14f0f593fd4e
Amirreza Asari <104520102+AmirrezaAsari@users.noreply.github.com>
2026-05-01 21:27:39 +0330

    feat: routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Posts\PublicPostController;
+use App\Http\Controllers\Posts\AdminPostController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    // Public routes
+    Route::prefix('posts')->group(function () {
+        Route::get('/', [PublicPostController::class, 'index'])->name('posts.index');
+        Route::get('/{slug}', [PublicPostController::class], 'show')->name('posts.show');
+
+    });
+
+
+    // Admin routes
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/posts', [AdminPostController::class, 'index'])->name('admin.posts.index');
+        Route::post('/posts', [AdminPostController::class, 'store'])->name('admin.posts.store');
+        Route::put('posts/{id}', [AdminPostController::class, 'update'])->name('admin.posts.update');
+        Route::delete('posts/{id}', [AdminPostController::class, 'destroy'])->name('admin.posts.destroy');
+    });
+
 });
