1787dde659a205dfd349ac8112058fa9758f0f3e
Amirreza Asari <104520102+AmirrezaAsari@users.noreply.github.com>
2026-05-02 12:10:31 +0330

    feat: controllers

--- app/Http/Controllers/Posts/AdminPostController.php
+++ app/Http/Controllers/Posts/AdminPostController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AdminPostController extends Controller
 {
-    //
+    public function index() {
+        return Post::latest()->paginate(15);
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'body' => 'required',
+        ]);
+
+        Post::create($validated);
+
+        return redirect()->route('admin.posts.index')->with('success', 'Post created!');
+    }
+
+
+    public function update(Request $request, $id) {
+        $post = Post::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts,slug,' . $id,
+            'body' => 'required',
+        ]);
+
+        $post->update($validated);
+
+        return redirect()->route('admin.posts.index')->with('success', 'Post updated!');
+    }
+
+    public function destroy($id) {
+        $post = Post::findOrFail($id);
+
+        $post->delete();
+
+        return redirect()->route('admin.posts.index')->with('success', 'Post deleted!');
+    }
 }
--- app/Http/Controllers/Posts/PublicPostController.php
+++ app/Http/Controllers/Posts/PublicPostController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PublicPostController extends Controller
 {
-    //
+    public function show($slug){
+        return Post::where('slug', $slug)->firstOrFail();
+    }
+
+    public function index()
+    {
+        return Post::latest()->paginate(15);
+    }
 }
